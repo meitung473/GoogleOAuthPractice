ae396e87962dc85c7bd5aefc87fd712865899b14
Rosa473 <yanshanhong0523@gmail.com>
2023-07-05 16:11:59 +0800

    Fix: UnhandledPromiseRejectionWarning

--- config/connect.js
+++ config/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+mongoose.Promise = global.Promise;
 module.exports = mongoose.connect(process.env.MONOGODB_CONNECT_URL).then(() => {
     console.log("connect database!");
 });
